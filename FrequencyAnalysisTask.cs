7a737f788e02252ae01d79df267e6d61fb4449f0
pavel <pavel@ENTERPRISE>
2023-01-07 21:34:11 +0300

    Добавьте файлы проекта.

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            var result = new Dictionary<string, string>(); // конечный результат
+            var wordThereList = new List<int>(); // лист для проверки ключ значений
+
+
+            foreach (var wordList in text) // перебераем листы в тексте
+            {
+                for (int words = 0; words < wordList.Count - 1; words++) // перебераем слова из листа для 2 разрядного слова
+                {
+                    var t = ByteMass(wordList[words]) + ByteMass(wordList[words + 1]);
+                    if (!wordThereList.Contains(t))
+                    {
+                        result.Add(wordList[words], wordList[words + 1]);
+                        wordThereList.Add(t);
+                    }
+                    else
+                        continue;
+                }
+                for (int words = 0; words < wordList.Count - 2; words++)// перебераем слова из листа для 3 разрядного слова
+                {
+                    var t = ByteMass(wordList[words]) + ByteMass(wordList[words + 1]) + ByteMass(wordList[words + 2]);
+                    if (!wordThereList.Contains(t))
+                    {
+                        result.Add(wordList[words] + " " + wordList[words + 1], wordList[words + 2]);
+                        wordThereList.Add(t);
+                    }
+                    else
+                        continue;
+                }
+            }
+
+            return result;
+        }
+        private static int ByteMass(string oneWord) // расчет массы слова
+        {
+            int t = 0;
+            foreach (var v in ASCIIEncoding.UTF8.GetBytes(oneWord))
+                t += v;
+            return t;
+        }
+    }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            return GetListSentences(text);
+        }
+
+        private static List<List<string>> GetListSentences(string text)
+        {
+            var separator = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentencesList = new List<List<string>>();
+
+            foreach (var sentence in text.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                sentencesList.Add(new List<string>(GetListWord(sentence)));
+
+                if (sentencesList.Last<List<string>>().Count == 0)
+                {
+                    sentencesList.Remove(sentencesList.Last<List<string>>());
+                }
+            }
+
+            return sentencesList;
+        }
+
+        private static List<string> GetListWord(string sentence)
+        {
+            var separator = new char[] {' '};
+            var wordList = new List<string>();
+
+            sentence = Regex.Replace(sentence,@"[^a-zA-Z']" , " ");
+            foreach (var word in sentence.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                wordList.Add(word.ToString().ToLower().Trim());
+            }
+
+            return wordList;
+        }
+    }
+}
